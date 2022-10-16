0bda675fdefd1c1fa5fc09224bf4e734032260a5
Somesh140 <somesh140@gmail.com>
2022-10-16 17:38:13 +0530

    update

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
-    description=" A small python package",
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
     long_description=long_description,
     long_description_content="text/markdown",
-    urls=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-
     },
-    package_dir={"":"src"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
